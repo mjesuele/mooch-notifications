0ea66788af9c36029444c99c975bfa9946ce8eb7
Matthew Jesuele <matt@mjesuele.com>
2015-11-28 18:17:26 -0800

    Added some pending describe blocks.

--- tests/shared/index.js
+++ tests/shared/index.js
@@ -1,5 +1,9 @@
 /* global describe it xdescribe */
 
 xdescribe('notifications', function() {
-
+  describe('adding notifications');
+  describe('removing notifications');
+  describe('clearing notifications');
+  describe('finding notifications');
+  describe('fetching notifications');
 });
